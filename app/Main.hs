82e4b9cf653b9913b9b815ee70e022a914a74e51
falgon <falgon53@gmail.com>
2018-08-02 14:46:34 +0900

    fix stupid specifications

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,10 @@ module Main where
 
 import Lib
 import System.Environment (getArgs)
+import Control.Monad ((<=<))
 
 main :: IO ()
 main = do
     p <- genRndSafePrime 16
     (pubkey, prikey) <- genKeys p $ rootFromSafePrime p
-    print . decode (pubkey, prikey) =<< encode pubkey . head =<< getArgs
+    mapM_ (print . decode (pubkey, prikey) <=< encode pubkey) =<< getArgs
